7f79715b15854987586979d86514d741fc8d9f1e
LoanTT <79430764+loanttty@users.noreply.github.com>
2021-04-24 12:44:07 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="icon" alt="logo" />
+      </header>
+      hello there!
+    </div>
+  );
+}
